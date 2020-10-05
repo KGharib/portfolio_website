0048b071ed85d82c8934bb628eba015891f09e87
KGharib <Khalid.Gharib1994@gmail.com>
2020-10-05 22:21:16 +0100

    -

--- app/__init__.py
+++ app/__init__.py
@@ -19,8 +19,9 @@ def favicon():
     return send_from_directory(os.path.join(app.root_path, 'static'),
     'favicon.ico', mimetype='image/vnd.microsoft.icon')
 
-@app.route('/index', methods=['POST', 'GET'])
+
 @app.route('/', methods=['POST', 'GET'])
+@app.route('/index')
 def index():
     return render_template('index.html')  
 
