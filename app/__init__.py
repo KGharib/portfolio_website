e62cde93aaf32be9abab639ae0adea1b57ea972f
KGharib <Khalid.Gharib1994@gmail.com>
2020-10-08 19:40:08 +0100

    Cleaned alignment

--- app/__init__.py
+++ app/__init__.py
@@ -7,9 +7,6 @@ from flask_cors import CORS
 app = Flask(__name__, template_folder='templates')
 CORS(app)
 app.static_folder = 'static'
-# app.template_folder='templates'
-# subscribers = []  
-
 
 myloader = jinja2.ChoiceLoader([app.jinja_loader,jinja2.FileSystemLoader(['templates']),])
 app.jinja_loader = myloader
@@ -18,12 +15,6 @@ app.jinja_loader = myloader
 def favicon():
     return send_from_directory(os.path.join(app.root_path, 'static'),
     'favicon.ico', mimetype='image/vnd.microsoft.icon')
-
-# @app.route('/index', methods=['POST', 'GET'])
-# @app.route('/', methods=['POST', 'GET'])
-# def index():
-#     return render_template('index.html')
-
 
 @app.route('/', methods=['POST', 'GET'])
 @app.route('/home')
