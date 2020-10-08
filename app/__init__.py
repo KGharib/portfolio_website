2e7008612c84a9db5708d042ebed5d32ae0732c9
KGharib <Khalid.Gharib1994@gmail.com>
2020-10-08 22:44:12 +0100

    added favicon

--- app/__init__.py
+++ app/__init__.py
@@ -14,7 +14,7 @@ app.jinja_loader = myloader
 @app.route('/favicon.ico')
 def favicon():
     return send_from_directory(os.path.join(app.root_path, 'static'),
-    'favicon.ico', mimetype='image/vnd.microsoft.icon')
+    'KG-Favicon2.ico', mimetype='image/vnd.microsoft.icon')
 
 @app.route('/', methods=['POST', 'GET'])
 @app.route('/home')
